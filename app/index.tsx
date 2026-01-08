cb18e336d96a882f077f7cec9b4a5501254a2490
radebeneo <radebeneo17@gmail.com>
2026-01-08 16:14:43 +0200

    setup custom styles, icons, images, fonts.

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
       <View className="flex-1 items-center justify-center bg-white">
-          <Text className="text-xl font-bold text-blue-500">
+          <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
               Welcome to Nativewind!
           </Text>
       </View>
