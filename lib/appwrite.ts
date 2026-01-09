c51d7519c10945502c0993954cc46d018dc16400
radebeneo <radebeneo17@gmail.com>
2026-01-09 12:31:16 +0200

    setup appwrtie config.

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.bhungane.bite",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
