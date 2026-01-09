2f682c69e9449d020af77446c067f96231ac546c
radebeneo <radebeneo17@gmail.com>
2026-01-09 11:17:13 +0200

    used ListHeaderComponent for scrollability.

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,63 @@
 import './globals.css'
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FlatList, Pressable, View, Text, Image} from "react-native";
-import {offers} from "@/constants"
+import {FlatList, Pressable, View, Text, Image, TouchableOpacity,} from "react-native";
+import {images, offers} from "@/constants"
 import { Fragment } from "react";
 import cn from 'clsx'
 
+
 export default function Index() {
   return (
-        <SafeAreaView>
-          <FlatList
-              data={offers}
-              renderItem={({ item, index }) => {
+        <SafeAreaView className="flex-1 bg-white">
 
-                  const isEven = index % 2 === 0
+                  <FlatList
+                      data={offers}
+                      renderItem={({ item, index }) => {
 
-                  return(
-                      <View>
-                          <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row' )} style={{backgroundColor: item.color}}>
-                              {({ pressed}) => (
-                                  <Fragment>
-                                      <View className={"h-full w-1/2"}>
-                                            <Image source={item.image} className={"size-full"} resizeMode={"contain"}/>
-                                      </View>
+                          const isEven = index % 2 === 0
 
-                                      <View className={"offer-card__info"}>
-                                          <Text>
-                                              {item.title}
-                                          </Text>
-                                      </View>
-                                  </Fragment>
-                              )}
-                          </Pressable>
-                      </View>
-                  )
-              }}
-          />
+                          return(
+                              <View>
+                                  <Pressable
+                                      className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row' )}
+                                      style={{backgroundColor: item.color}}
+                                      android-ripple={{ color: "#fffff22"}}
+                                  >
+                                      {({ pressed}) => (
+                                          <Fragment>
+                                              <View className={"h-full w-1/2"}>
+                                                    <Image source={item.image} className={"size-full"} resizeMode={"contain"}/>
+                                              </View>
+
+                                              <View className={cn("offer-card__info", isEven ? 'pl-10':'pr-10')}>
+                                                  <Text className="h1-bold text-white leading tight">
+                                                      {item.title}
+                                                  </Text>
+                                                  <Image source={images.arrowRight} className="size-10" resizeMode="contain" tintColor="#ffffff"/>
+                                              </View>
+                                          </Fragment>
+                                      )}
+                                  </Pressable>
+                              </View>
+                          )
+                      }}
+                      contentContainerClassName="pb-28 px-5"
+
+                      ListHeaderComponent={() => (
+                          <View className="flex-between flex-row w-full my-5">
+                              <View className="flex-start">
+                                  <Text className="small-bold text-primary">DELIVER TO</Text>
+                                  <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                                      <Text className="paragraph-bold text-dark-100">Soweto</Text>
+                                      <Image source={images.arrowDown} className="size-3" resizeMode="contain" />
+                                  </TouchableOpacity>
+                              </View>
+
+                              <Text>Cart</Text>
+                          </View>
+                      )}
+                  />
+
       </SafeAreaView>
   );
 }
