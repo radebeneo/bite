67723306fe9b3ac991661f0b8f05ef8f246eda23
radebeneo <radebeneo17@gmail.com>
2026-01-09 09:51:10 +0200

    added global styles, and rendered item card an images in home screen.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,36 @@
 import './globals.css'
-import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Pressable, View, Text, Image} from "react-native";
+import {offers} from "@/constants"
+import { Fragment } from "react";
 
 export default function Index() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-          <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
-              Welcome to Nativewind!
-          </Text>
-      </View>
+        <SafeAreaView>
+          <FlatList
+              data={offers}
+              renderItem={({ item, index }) => {
+                  return(
+                      <View>
+                          <Pressable className="offer-card" style={{backgroundColor: item.color}}>
+                              {({ pressed}) => (
+                                  <Fragment>
+                                      <View className={"h-full w-1/2"}>
+                                            <Image source={item.image} className={"size-full"} resizeMode={"contain"}/>
+                                      </View>
+
+                                      <View className={"offer-card__info"}>
+                                          <Text>
+                                              {item.title}
+                                          </Text>
+                                      </View>
+                                  </Fragment>
+                              )}
+                          </Pressable>
+                      </View>
+                  )
+              }}
+          />
+      </SafeAreaView>
   );
 }
