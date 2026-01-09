5e4e6e3077d47dd216fb44424742938b40afaecd
radebeneo <radebeneo17@gmail.com>
2026-01-09 16:24:48 +0200

    completed custom button for auth pages.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,10 +1,27 @@
-import {View, Text} from 'react-native'
+import {View, Text, TouchableOpacity} from 'react-native'
+import cn from 'clsx'
 
-const CustomButton = () => {
+import {CustomButtonProps} from "@/type"
+
+const CustomButton = ({
+    onPress,
+    title="Click Me",
+    style,
+    textStyle,
+    leftIcon,
+    isLoading = false
+}: CustomButtonProps) => {
     return (
-        <View>
-            <Text>CustomButton</Text>
-        </View>
+        <TouchableOpacity className={cn('custom-btn', style)} onPress={onPress}>
+            {leftIcon}
+            <View className="flex-center flex-row">
+                {isLoading ? (
+                    <ActivityIndicator size="lsmall" color="white" />
+                ): (
+                    <Text className={cn('text-white-100 paragraph-semibold', textStyle)}>{title}</Text>
+                )}
+            </View>
+        </TouchableOpacity>
     )
 }
 export default CustomButton
