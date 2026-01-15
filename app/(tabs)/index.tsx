6bfd3090d816fe5d0d6b10cf4945f6328a4d7470
radebeneo <radebeneo17@gmail.com>
2026-01-15 15:37:22 +0200

    implemented tab navigation.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,9 +14,6 @@ export default function Index() {
     
     const {user} = useAuthStore()
 
-    console.log("user;", JSON.stringify(user, null, 2))
-
-
     return (
         <SafeAreaView className="flex-1 bg-white">
 
