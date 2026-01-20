a8050977563307b09475cad1d46493ddb5ac950c
radebeneo <radebeneo17@gmail.com>
2026-01-20 21:50:04 +0200

    added hooks to fetch data in the search page.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,27 @@
 import {View, Text, Button} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
+import {useLocalSearchParams} from "expo-router";
 import seed from "@/lib/seed";
+import useAppwrite from "@/lib/useAppwrite";
+import {getCategories, getMenu} from "@/lib/appwrite";
+import {useEffect} from "react";
+
 
 
 
 const Search = () => {
+
+    const {category, query} = useLocalSearchParams<{query: string, category: string}> ()
+
+    const { data, refetch, loading } = useAppwrite({fn: getMenu, params: { category,  query, limit: 6,}})
+
+    const {data: categories} = useAppwrite({fn: getCategories})
+
+    useEffect(() => {
+        refetch({category, query, limit: 6})
+    }, [category, query]);
+
+
     return (
         <SafeAreaView>
             <Text>Search</Text>
