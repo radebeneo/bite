7e1cb55ecef832023c43f750ae6e3c1fcbfe01ed
radebeneo <radebeneo17@gmail.com>
2026-01-26 22:23:17 +0200

    starting to implement filter component.

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,11 +1,26 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, FlatList} from 'react-native'
+import React, {useState} from 'react'
+import {Category} from "@/type";
+import {useLocalSearchParams} from "expo-router";
 
-const Filter = () => {
+const Filter = ({categories}: {categories: Category[]}) => {
+
+    const searchParams = useLocalSearchParams()
+
+    const [active, setActive] = useState(searchParams.category || '')
+
+    const handlePress = (id: string) => {}
+
+    const filterData: (Category | {$id: string; name: string})[] = categories ? [{$id: 'all', name: 'All'}, ...categories] : [{ $id: 'all', name: 'All'}]
+
     return (
-        <View>
-            <Text>Filter</Text>
-        </View>
+        <FlatList
+        data={filterData}
+        keyExtractor={item => item.$id}
+        renderItem={}
+
+        />
+
     )
 }
 export default Filter
