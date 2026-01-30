cbd81f30294a876ed5767205b150b929302571b4
radebeneo <radebeneo17@gmail.com>
2026-01-30 16:02:26 +0200

    completed custom header component and implemented it in the cart page.

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,27 @@
-import {View, Text} from 'react-native'
-
+import {View, Text, FlatList} from 'react-native'
+import {SafeAreaView} from "react-native-safe-area-context";
+import {useCartStore} from "@/store/cart.store";
+import CustomHeader from "@/components/CustomHeader"
 
 const Cart = () => {
+
+    const {items, getTotalItems, getTotalPrice} = useCartStore()
+
+    const totalItems = getTotalItems()
+    const totalPrice = getTotalPrice()
+
+
     return (
-        <View>
-            <Text>Cart</Text>
-        </View>
+        <SafeAreaView className="bg-white h-full ">
+            <FlatList
+                data={items}
+                renderItem={({item}) => <Text>Cart Item</Text>}
+                keyExtractor={item => item.$id}
+                contentContainerClassName="px-5 pt-5 pb-38"
+                ListHeaderComponent={() => <CustomHeader title="YourCart"/>}
+                ListEmptyComponent={() => <Text className="text-center">No items in your cart</Text>}
+            />
+        </SafeAreaView>
     )
 }
 export default Cart
